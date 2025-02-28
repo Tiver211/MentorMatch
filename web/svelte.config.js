d31b6d6b99bf7374bc167ea42c1bd9e9e4f85139
pakhrom <ro.pakhomov@gmail.com>
2025-02-28 21:22:53 +0300

    Update scss preprocessing to use vite-plugin-svelte instead of svelte-preprocess

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,12 +1,11 @@
 import adapter from '@sveltejs/adapter-auto';
-import { scss } from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [scss(), vitePreprocess()],
+	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter()
