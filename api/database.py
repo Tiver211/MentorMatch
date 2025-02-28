1a8567d692c9374cb41b00d4afb03ed84276bc6d
EvdokDima <yoto8644@gmail.com>
2025-03-01 02:49:15 +0500

    rename src to api, add docker-compose

---
+++ api/database.py
@@ -0,0 +1,32 @@
+from sqlalchemy.dialects.postgresql import UUID as UUIDP
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from uuid import UUID
+import os
+
+Base = declarative_base()
+
+DATABASE_URL = os.getenv("POSTGRES_CONN")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class User_table(Base):
+    __tablename__ = "users"
+
+    user_id: UUID = Column(UUIDP(as_uuid=True), primary_key=True)
+    first_name: str = Column(String, nullable=False)
+    last_name: str = Column(String, nullable=False)
+    age: int = Column(Integer, nullable=False)
+    school: str = Column(String, nullable=False)
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
