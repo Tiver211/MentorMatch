e9d7ba8a60adb77c8c19a5ca52de2f75ccbfd54b
EvdokDima <yoto8644@gmail.com>
2025-03-01 17:51:25 +0500

    user endpoint

--- api/user/user_router.py
+++ api/user/user_router.py
@@ -1,34 +1,7 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
-from datetime import datetime, timedelta
-import jwt
-from fastapi.responses import JSONResponse
-from ..database import get_db, User_table
-from uuid import uuid4
-
-from base_model import User
+from sign_in.user_auth_sign_in_post import user_auth_sign_in_router
+from sign_up.user_auth_post import user_auth_post_router
+from fastapi import APIRouter
 
 user_router = APIRouter()
-
-@user_router.post("/user/auth")
-def post_user(user: User, db: Session = Depends(get_db)):
-    new_user = User_table(
-        user_id=uuid4(),
-        first_name=user.first_name,
-        last_name=user.last_name,
-        age=user.age,
-        school=user.school
-    )
-
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    token_payload = {
-        "sub": str(new_user.user_id),
-        "exp": datetime.utcnow() + timedelta(hours=1)
-    }
-    token = jwt.encode(token_payload, "gsugfsgoGYOUF646", algorithm='HS256')
-
-    return JSONResponse(status_code=201, content={"token": token})
-
+user_router.include_router(user_auth_post_router)
+user_router.include_router(user_auth_sign_in_router)
