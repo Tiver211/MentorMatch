8f97c2fba0b5822c29b770916991a70546843b1b
pakhrom <ro.pakhomov@gmail.com>
2025-03-01 18:13:37 +0300

    Add MentorsList component

---
+++ web/src/lib/people-lists/mentors-list.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	let mentors = [
+		{ name: 'Хто я?', age: 19, about: 'Гейдизайнер и разработчик на Godot' },
+		{ name: 'Человек', age: 999, about: 'Профессиональный бездельник' }
+	];
+</script>
+
+<ul>
+	{#each mentors as { name, age, about }}
+		<li>
+			<b>{name}</b>
+			<p>{age} лет</p>
+			<p>
+				<b>О себе:</b> <br />
+				{about}
+			</p>
+		</li>
+	{/each}
+</ul>
+
+<style>
+	ul {
+		padding: 1em 0;
+	}
+
+	li {
+		padding: 0.5em;
+
+		border: 1px solid black;
+		border-radius: 0.5em;
+
+		list-style: none;
+	}
+
+	p {
+		margin-bottom: 0.1em;
+	}
+</style>
