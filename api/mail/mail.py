f8c51ebe6e3ea10889021bfa8b476847257f83b5
tiver211 <qwertykirzik@gmail.com>
2025-03-02 00:11:38 +0300

    create email

---
+++ api/mail/mail.py
@@ -0,0 +1,20 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+
+SMTP_HOST = "mailhog"
+SMTP_PORT = 1025
+SMTP_USER = "noreply"
+SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
+
+def send_email(target: str, title: str, body: str):
+    msg = MIMEText(body)
+    msg['Subject'] = title
+    msg['From'] = SMTP_USER
+    msg['To'] = target
+
+    with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as server:
+        server.starttls()
+        server.login(SMTP_USER, SMTP_PASSWORD)
+        server.sendmail(SMTP_USER, [target], msg.as_string())
+
