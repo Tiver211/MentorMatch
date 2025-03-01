ad4fe0e0203c906ded404bd09a8429ffc30d8061
EvdokDima <yoto8644@gmail.com>
2025-03-01 19:53:24 +0500

    fixes

--- api/user/user_router.py
+++ api/user/user_router.py
@@ -1,7 +1,9 @@
-from sign_in.user_auth_sign_in_post import user_auth_sign_in_router
-from sign_up.user_auth_post import user_auth_post_router
+from .sign_in.user_auth_sign_in_post import user_auth_sign_in_router
+from .sign_up.user_auth_post import user_auth_post_router
+from .update.user_data_update import user_update_data_patch
 from fastapi import APIRouter
 
 user_router = APIRouter()
+user_router.include_router(user_update_data_patch)
 user_router.include_router(user_auth_post_router)
 user_router.include_router(user_auth_sign_in_router)
