1b95880220456fb687e5adac0b0af7752c3f5469
EvdokDima <yoto8644@gmail.com>
2025-03-02 02:15:56 +0500

    fix bytes

--- api/offer/post_offer/post_offer.py
+++ api/offer/post_offer/post_offer.py
@@ -1,11 +1,15 @@
 import os
+import random
 
 import jwt
 from fastapi import APIRouter, Depends, Header
 from sqlalchemy.orm import Session
+from starlette.responses import JSONResponse
 
 from .base_model import Offer
-from ...database import get_db, User_table, Mentor_table
+from ...database import get_db, User_table
+
+from api.SMTP.init_server import send_email
 
 post_offer_router = APIRouter()
 
@@ -16,5 +20,8 @@ def post_offer(offer: Offer, db: Session = Depends(get_db), authorization: str =
     user_data = jwt.decode(token, os.getenv("RANDOM_SECRET"), algorithms=['HS256'])
 
     user = db.query(User_table).filter(User_table.user_id == user_data["user_id"])
-    mentor = db.query(Mentor_table).filter(Mentor_table.login == offer.mentor_login).first()
+    mentor = db.query(User_table).filter(User_table.login == offer.mentor_login).first()
 
+    send_email("New offer", f"You have received an offer from {user.contact}", mentor.contact)
+
+    return JSONResponse(status_code=200, content={"status": "ok"})
