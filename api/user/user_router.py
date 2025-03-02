542ea27f5969d3e1c7b0c0a4e580816686aad914
tiver211 <qwertykirzik@gmail.com>
2025-03-02 11:19:43 +0300

    fix route include

--- api/user/user_router.py
+++ api/user/user_router.py
@@ -3,8 +3,10 @@ from fastapi import APIRouter
 from .sign_in.user_auth_sign_in_post import user_auth_sign_in_router
 from .sign_up.user_auth_post import user_auth_post_router
 from .update.user_data_update import user_update_data_patch
+from .verification.user_verification import user_verification
 
 user_router = APIRouter()
 user_router.include_router(user_update_data_patch)
 user_router.include_router(user_auth_post_router)
 user_router.include_router(user_auth_sign_in_router)
+user_router.include_router(user_verification)
