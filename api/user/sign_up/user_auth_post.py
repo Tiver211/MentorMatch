aef52df49a1da9cd1c0d4b676fede5ad1fd931d5
tiver211 <qwertykirzik@gmail.com>
2025-03-02 10:58:55 +0300

    added verify

--- api/user/sign_up/user_auth_post.py
+++ api/user/sign_up/user_auth_post.py
@@ -1,20 +1,22 @@
 import os
+import secrets
 from datetime import datetime, timedelta
 from uuid import uuid4
 
 import bcrypt
 import jwt
+import redis
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
 from api.user.sign_up.base_model import User
-from ...database import get_db, User_table
+from ...database import get_db, User_table, get_cache
 
 user_auth_post_router = APIRouter()
 
 @user_auth_post_router.post("/user/auth/sign-up")
-def post_user(user: User, db: Session = Depends(get_db)):
+def post_user(user: User, db: Session = Depends(get_db), redis_client: redis.Redis = Depends(get_cache())):
     user_db = db.query(User_table).filter(User_table.login == user.login).first()
     if user_db:
         return JSONResponse(status_code=409, content={"status": "Login already exists"})
@@ -29,25 +31,17 @@ def post_user(user: User, db: Session = Depends(get_db)):
         last_name=user.last_name,
         age=user.age,
         about=user.about,
-        contact=user.contact
+        contact=user.contact,
+        is_active = False
     )
 
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
 
-    data = \
-        {
-            "user_id": str(new_user.user_id),
-            "login": new_user.login
-        }
+    verify_token = secrets.token_urlsafe(32)
 
-    token_payload = {
-        "sub": data,
-        "exp": datetime.utcnow() + timedelta(days=365)
-    }
+    redis_client.set(verify_token, user.id)
 
-    token = jwt.encode(token_payload, os.getenv("RANDOM_SECRET"), algorithm='HS256')
-
-    return JSONResponse(status_code=201, content={"token": token})
+    return JSONResponse(status_code=201, content={"status": "ok"})
 
