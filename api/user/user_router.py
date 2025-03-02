7e46d01b2de850aea8b4cb2ca7e5bfc97d297eed
tiver211 <qwertykirzik@gmail.com>
2025-03-03 01:33:40 +0300

    fix

--- api/user/user_router.py
+++ api/user/user_router.py
@@ -9,6 +9,7 @@ from .get_offer_by_id.get_offer_by_id import get_offer_by_id_router
 from .get_profile.get_profile import get_profile_router
 from .get_mentors_list.get_mentors_list import get_mentors_router
 from .get_mentor_by_id.get_mentors_by_id import get_mentor_router
+from .avatar.get_avatar import get_avatar_router
 
 user_router = APIRouter()
 user_router.include_router(user_update_data_patch)
@@ -20,3 +21,4 @@ user_router.include_router(get_mentor_router)
 user_router.include_router(get_profile_router)
 user_router.include_router(get_mentors_router)
 user_router.include_router(get_offers_router)
+user_router.include_router(get_avatar_router)
