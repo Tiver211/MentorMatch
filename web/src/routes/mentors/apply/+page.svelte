a7ff28e495cb1b15a341a8244257cd1deb543223
pakhrom <ro.pakhomov@gmail.com>
2025-03-02 16:43:43 +0300

    feat(web): Update /mentors/apply layout

--- web/src/routes/mentors/apply/+page.svelte
+++ web/src/routes/mentors/apply/+page.svelte
@@ -2,14 +2,16 @@
 	let loggedIn = localStorage.getItem('loggedIn') ? true : false;
 </script>
 
-<h1>Заявка на становление ментором</h1>
-
 {#if loggedIn}
+	<h1>Заявка на становление ментором</h1>
 	<form action="" method="POST">
 		<h3>В чем специализируютесь?</h3>
 		<textarea name="direction" id="direction"></textarea>
 		<button type="submit">Подать заявку</button>
 	</form>
 {:else}
-	<h3>Для становления ментором необходимо <a href="/signup">зарегистрироваться</a>.</h3>
+	<h1>Необходима регистрация</h1>
+	<h3>
+		Для отправки заявки на становление ментором необходимо <a href="/signup">зарегистрироваться</a>.
+	</h3>
 {/if}
