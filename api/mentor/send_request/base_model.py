bb4808836a5be6017eeef2c9c817eacf6c7827a9
EvdokDima <yoto8644@gmail.com>
2025-03-02 20:18:01 +0500

    add create mentor for admin

--- api/mentor/send_request/base_model.py
+++ api/mentor/send_request/base_model.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel, List, Field
-
+from pydantic import BaseModel, Field
+from typing import List
 
 class Request(BaseModel):
     about: str = Field(min_length=100, max_length=2000)
