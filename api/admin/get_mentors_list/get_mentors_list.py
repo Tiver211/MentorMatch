6caa7d801e88432129bd0f11772f83d498e96d1a
EvdokDima <yoto8644@gmail.com>
2025-03-02 05:00:41 +0500

    add admin

---
+++ api/admin/get_mentors_list/get_mentors_list.py
@@ -0,0 +1,66 @@
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import asc, desc
+from fastapi import APIRouter, Query
+from typing import Optional
+
+from starlette.responses import JSONResponse
+
+from ...database import get_db, User_table, Mentor_table
+
+get_mentors_router = APIRouter()
+
+@get_mentors_router.get("admin/secreturl/mentors")
+def get_mentors(
+    db: Session = Depends(get_db),
+    sort_by: Optional[str] = Query(None),
+    direction: Optional[str] = Query(None),
+    age_from: Optional[int] = Query(0),
+    age_to: Optional[int] = Query(999),
+    order: Optional[str] = Query('asc')
+):
+    query = db.query(Mentor_table).join(User_table).filter(User_table.age <= age_to, User_table.age >= age_from)
+
+    if not direction:
+        if sort_by == "name":
+            if order == 'asc':
+                query = query.order_by(asc(User_table.first_name), asc(User_table.last_name)).filter(User_table.age <= age_to, User_table.age >= age_from)
+            else:
+                query = query.order_by(desc(User_table.first_name), desc(User_table.last_name)).filter(User_table.age <= age_to, User_table.age >= age_from)
+        elif sort_by == "age":
+            if order == 'asc':
+                query = query.order_by(asc(User_table.age)).filter(User_table.age <= age_to, User_table.age >= age_from)
+            else:
+                query = query.order_by(desc(User_table.age)).filter(User_table.age <= age_to, User_table.age >= age_from)
+    else:
+        if sort_by == "name":
+            if order == 'asc':
+                query = query.order_by(asc(User_table.first_name), asc(User_table.last_name)).filter(
+                    User_table.age <= age_to, User_table.age >= age_from, Mentor_table.direction == direction)
+            else:
+                query = query.order_by(desc(User_table.first_name), desc(User_table.last_name)).filter(
+                    User_table.age <= age_to, User_table.age >= age_from, Mentor_table.direction == direction)
+        elif sort_by == "age":
+            if order == 'asc':
+                query = query.order_by(asc(User_table.age)).filter(User_table.age <= age_to, User_table.age >= age_from, Mentor_table.direction == direction)
+            else:
+                query = query.order_by(desc(User_table.age)).filter(User_table.age <= age_to,
+                                                                    User_table.age >= age_from, Mentor_table.direction == direction)
+
+    mentors = query.all()
+
+    result = [
+        {
+            "mentor_id": str(mentor.mentor_id),
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "age": user.age,
+            "direction": mentor.direction,
+            "about": user.about,
+            "contact": user.contact
+        }
+        for mentor in mentors
+        for user in [db.query(User_table).filter(User_table.user_id == mentor.user_id).one()]
+    ]
+
+    return JSONResponse(status_code=200, content=result)
--- api/offer/post_offer/post_offer.py
+++ api/offer/post_offer/post_offer.py
@@ -1,5 +1,4 @@
 import os
-import random
 
 import jwt
 from fastapi import APIRouter, Depends, Header
@@ -7,7 +6,7 @@ from sqlalchemy.orm import Session
 from starlette.responses import JSONResponse
 
 from .base_model import Offer
-from ...database import get_db, User_table
+from ...database import get_db, User_table, Offer_table, Mentor_table
 
 from api.SMTP.init_server import send_email
 
@@ -20,7 +19,12 @@ def post_offer(offer: Offer, db: Session = Depends(get_db), authorization: str =
     user_data = jwt.decode(token, os.getenv("RANDOM_SECRET"), algorithms=['HS256'])
 
     user = db.query(User_table).filter(User_table.user_id == user_data["user_id"])
-    mentor = db.query(User_table).filter(User_table.login == offer.mentor_login).first()
+    mentor = db.query(User_table).join(Mentor_table).filter(User_table.login == offer.mentor_login, User_table.user_id == Mentor_table.user_id).first()
+
+    new_offer = Offer_table(mentor_id=mentor.mentor_id, user_id=user.user_id, message=offer.message)
+
+    db.add(new_offer)
+    db.commit()
 
     send_email("New offer", f"You have received an offer from {user.contact}", mentor.contact)
 
