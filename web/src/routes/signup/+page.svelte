0baa97e58c15d3cec3d43863e2b79d5b45f8fa57
pakhrom <ro.pakhomov@gmail.com>
2025-03-02 15:13:32 +0300

    web: Add email verification to registration form

--- web/src/routes/signup/+page.svelte
+++ web/src/routes/signup/+page.svelte
@@ -1,41 +1,70 @@
+<script lang="ts">
+	let verificationAwaiting = $state(false);
+</script>
+
 <h1>Регистрация пользователя</h1>
-<form action="/signup" method="POST">
-	<h3>Личная информация</h3>
-	<fieldset class="grid">
-		<label for="first-name">
-			Имя*
-			<input type="text" name="first-name" id="first-name" required />
+
+{#if !verificationAwaiting}
+	<form action="/signup" method="POST">
+		<h3>Личная информация</h3>
+		<fieldset class="grid">
+			<label for="first-name">
+				Имя*
+				<input type="text" name="first-name" id="first-name" required />
+			</label>
+			<label for="last-name">
+				Фамилия
+				<input type="text" name="last-name" id="last-name" />
+			</label>
+			<label for="age">
+				Возраст
+				<input type="number" name="age" id="age" pattern="^(0?[0-9]?[0-9]|1[01][0-9]|120)$" />
+			</label>
+		</fieldset>
+		<label for="about">
+			<h3>Напишите немного информации о себе</h3>
+			<textarea name="about" id="about"></textarea>
 		</label>
-		<label for="last-name">
-			Фамилия
-			<input type="text" name="last-name" id="last-name" />
+		<label for="contact">
+			<h3>Как с вами можно связаться?</h3>
+			<textarea name="contact" id="contact"></textarea>
 		</label>
-		<label for="age">
-			Возраст
-			<input type="number" name="age" id="age" pattern="^(0?[0-9]?[0-9]|1[01][0-9]|120)$" />
-		</label>
-	</fieldset>
-	<label for="about">
-		<h3>Напишите немного информации о себе</h3>
-		<textarea name="about" id="about"></textarea>
-	</label>
-	<label for="contact">
-		<h3>Как с вами можно связаться?</h3>
-		<textarea name="contact" id="contact"></textarea>
-	</label>
-	<h3>Данные авторизации</h3>
-	<fieldset class="grid">
-		<label for="email">
-			Электронная почта*
-			<input type="email" name="email" id="email" required />
-		</label>
-		<label for="password">
-			Пароль*
-			<input type="password" name="password" id="password" required />
-		</label>
-	</fieldset>
-	<button type="submit">Завершить регистрацию</button>
-</form>
+		<h3>Данные авторизации</h3>
+		<fieldset class="grid">
+			<label for="email">
+				Электронная почта*
+				<input type="email" name="email" id="email" required />
+			</label>
+			<label for="password">
+				Пароль*
+				<input type="password" name="password" id="password" required />
+			</label>
+		</fieldset>
+		<button
+			type="submit"
+			onclick={() => {
+				verificationAwaiting = true;
+				window.scrollTo(0, 0);
+			}}>Завершить регистрацию</button
+		>
+	</form>
+{:else}
+	<article>
+		<h3>Верификация почты</h3>
+		<p>
+			Перейдите по ссылке в письме, отправленной вам на почту, указанную в форме регистрации. Если
+			не можете найти письмо, проверьте папку «Спам».
+		</p>
+		<input type="button" value="Отправить письмо заново" onclick={() => {}} />
+		<input
+			type="button"
+			value="Проверить верификацию"
+			onclick={() => {
+				localStorage.setItem('loggedIn', 'true');
+			}}
+		/>
+	</article>
+{/if}
 
 <style>
 	fieldset {
