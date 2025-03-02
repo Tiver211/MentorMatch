ace9a3a25a71e57d35832934aa818e27a7282d5c
tiver211 <qwertykirzik@gmail.com>
2025-03-02 17:39:04 +0300

    temp fix

--- api/user/sign_up/user_auth_post.py
+++ api/user/sign_up/user_auth_post.py
@@ -45,9 +45,9 @@ def post_user(user: User, db: Session = Depends(get_db), redis_client: redis.Red
     redis_client.set(verify_token, str(new_user.user_id))
 
     verify_url = f"https://prod-team-35-lg7sic6v.final.prodcontest.ru/user/verify?token={verify_token}"
-    send_email(user.contact,
-               "Верификация почты",
-               f"пожалуйста подтвердите вашу почту, для этого перейдите по ссылке: {verify_url}",)
+    #send_email(user.contact,
+    #           "Верификация почты",
+    #           f"пожалуйста подтвердите вашу почту, для этого перейдите по ссылке: {verify_url}",)
 
-    return JSONResponse(status_code=201, content={"status": "ok"})
+    return JSONResponse(status_code=201, content={"status": "ok", "verify_email": verify_url})
 
