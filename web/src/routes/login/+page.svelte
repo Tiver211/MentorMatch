15471f6bd9909bfaa1fcb941a80d576982bba7f9
pakhrom <ro.pakhomov@gmail.com>
2025-03-02 17:59:19 +0300

    feat(web): Add /login

---
+++ web/src/routes/login/+page.svelte
@@ -0,0 +1,15 @@
+<hgroup>
+	<h1>Вход в аккаунт</h1>
+	<p>Нет аккаунта? <a href="/signup">Зарегистрируйтесь</a>.</p>
+</hgroup>
+<form action="POST">
+	<label for="email">
+		Электронная почта
+		<input type="email" name="email" id="email" />
+	</label>
+	<label for="password">
+		Пароль
+		<input type="password" name="password" id="password" />
+	</label>
+	<button type="submit">Войти в аккаунт</button>
+</form>
