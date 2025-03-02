e6ad33a7d123681a92749c4f29fd3ae1cc69b2b5
pakhrom <ro.pakhomov@gmail.com>
2025-03-02 15:16:05 +0300

    web: Add application for mentoring

--- web/src/routes/mentors/apply/+page.svelte
+++ web/src/routes/mentors/apply/+page.svelte
@@ -1,1 +1,15 @@
+<script lang="ts">
+	let loggedIn = localStorage.getItem('loggedIn') ? true : false;
+</script>
+
 <h1>Заявка на становление ментором</h1>
+
+{#if loggedIn}
+	<form action="" method="POST">
+		<h3>В чем специализируютесь?</h3>
+		<textarea name="direction" id="direction"></textarea>
+		<button type="submit">Подать заявку</button>
+	</form>
+{:else}
+	<h3>Для становления ментором необходимо <a href="/signup">зарегистрироваться</a>.</h3>
+{/if}
