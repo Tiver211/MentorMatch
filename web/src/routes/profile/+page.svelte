312f8776f99be863195db1bc5f48872e09a7a57f
pakhrom <ro.pakhomov@gmail.com>
2025-03-02 18:39:48 +0300

    debug(web): add debug button for receiving admin access

--- web/src/routes/profile/+page.svelte
+++ web/src/routes/profile/+page.svelte
@@ -7,3 +7,28 @@
 		window.location.href = '/';
 	}}
 />
+<h6 class="red">
+	НЕ ДЛЯ ПРОДА! ЕСЛИ ЭТА КНОПКА ОСТАНЕТСЯ ЗДЕСЬ ПОСЛЕ ДД ВСЕМ РУКИ В БАРАНКУ СВЕРНУ!!!!!!!!!!
+</h6>
+<input
+	type="button"
+	id="hehehe"
+	value="Выдать админку бесплатно, без регистрации и смс"
+	onclick={() => {
+		localStorage.setItem('isAdmin', 'true');
+	}}
+/>
+
+<style>
+	.red {
+		padding: 0.25em;
+		border-radius: 0.25em;
+
+		color: white;
+		background-color: red;
+	}
+
+	#hehehe {
+		background-color: green;
+	}
+</style>
