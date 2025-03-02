3881a91c86c1ccc382d88d6d953d59351573e0ae
pakhrom <ro.pakhomov@gmail.com>
2025-03-02 15:13:32 +0300

    web: Add email verification to registration form

---
+++ web/src/routes/verification/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { page } from '$app/state';
+
+	let registrationToken: string | null;
+	if (browser) {
+		registrationToken = page.url.searchParams.get('token');
+		if (!registrationToken) window.location.href = '/';
+	}
+</script>
+
+{#if registrationToken}
+	// Проверка токена
+{:else}
+	<article>
+		<h1>Отсутствует токен</h1>
+		<p>Возвращаю на главную</p>
+	</article>
+{/if}
