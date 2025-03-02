0ff55185a269ad01a5704c25e3cf452aa4ae52d7
EvdokDima <yoto8644@gmail.com>
2025-03-02 23:10:18 +0500

    fix update

--- api/user/update/base_model.py
+++ api/user/update/base_model.py
@@ -1,9 +1,9 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class User(BaseModel):
-    first_name: Optional[str]= None
-    last_name: Optional[str] = None
-    about: Optional[str] = None
+    first_name: str = Field(min_length=2)
+    last_name: str = Field(min_length=2)
+    about: Optional[str] = Field(default=None, min_length=100, max_length=200)
