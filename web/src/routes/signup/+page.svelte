3a3c55cad88a9e00cb7bd55b6a6cc34be19e5be5
pakhrom <ro.pakhomov@gmail.com>
2025-03-03 20:33:55 +0300

    feat(web): implement signing up ability (haven't been tested yet)

--- web/src/routes/signup/+page.svelte
+++ web/src/routes/signup/+page.svelte
@@ -1,17 +1,51 @@
 <script lang="ts">
-	import { watch } from 'runed';
+	import type { userSchema } from '$lib/schemas';
+	import { z } from 'zod';
 
-	let isVerificationWindowShown = $state(false);
-	let resendTimer = $state(60);
-
-	watch([() => isVerificationWindowShown], () => {
-		if (isVerificationWindowShown) {
-			window.scrollTo(0, 0);
-			setInterval(() => {
-				resendTimer--;
-			}, 1000);
-		}
+	type User = z.infer<typeof userSchema>;
+	let user: User = $state({
+		login: '',
+		password: '',
+		first_name: '',
+		last_name: undefined,
+		age: undefined,
+		about: undefined,
+		contact: undefined
 	});
+
+	let validationErrorWarning: HTMLParagraphElement | undefined = $state();
+	let otherErrorWarning: HTMLParagraphElement | undefined = $state();
+	let isVerificationWindowShown = $state(false);
+
+	let promise: any = $state();
+	const sendForm = (event: Event) => {
+		event.preventDefault();
+		promise = undefined;
+		promise = fetch('https://prod-team-35-lg7sic6v.final.prodcontest.ru/api/user/auth/sign-up', {
+			method: 'POST',
+			body: JSON.stringify(user),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8'
+			}
+		}).then(async (response) => {
+			if (!response.ok) {
+				console.log(
+					response.json().then((value) => {
+						value.details.msg;
+					})
+				);
+
+				const responseJson = await response.json();
+
+				if (response.status === 422 && validationErrorWarning) {
+					validationErrorWarning.textContent = 'Данные некорректны: ' + responseJson.details.msg;
+					validationErrorWarning.hidden = false;
+				}
+				throw new Error('');
+			}
+			isVerificationWindowShown = true;
+		});
+	};
 </script>
 
 {#if !isVerificationWindowShown}
@@ -19,47 +53,69 @@
 		<h1>Регистрация пользователя</h1>
 		<p>Уже есть аккаунт? <a href="/login">Войдите</a> в него.</p>
 	</hgroup>
-	<form action="/signup" method="POST">
+	<form onsubmit={sendForm}>
 		<h3>Личная информация</h3>
 		<fieldset class="grid">
 			<label for="first-name">
 				Имя*
-				<input type="text" name="first-name" id="first-name" required />
+				<input
+					type="text"
+					name="first-name"
+					id="first-name"
+					bind:value={user.first_name}
+					required
+				/>
 			</label>
 			<label for="last-name">
 				Фамилия
-				<input type="text" name="last-name" id="last-name" />
+				<input type="text" name="last-name" id="last-name" bind:value={user.last_name} />
 			</label>
 			<label for="age">
 				Возраст
-				<input type="number" name="age" id="age" pattern="^(0?[0-9]?[0-9]|1[01][0-9]|120)$" />
+				<input
+					type="number"
+					name="age"
+					id="age"
+					pattern="^(0?[0-9]?[0-9]|1[01][0-9]|120)$"
+					bind:value={user.age}
+				/>
 			</label>
 		</fieldset>
 		<label for="about">
 			<h3>Напишите немного информации о себе</h3>
-			<textarea name="about" id="about"></textarea>
+			<textarea name="about" id="about" bind:value={user.about}></textarea>
 		</label>
 		<label for="contact">
 			<h3>Как с вами можно связаться?</h3>
-			<textarea name="contact" id="contact"></textarea>
+			<textarea name="contact" id="contact" bind:value={user.contact}></textarea>
 		</label>
 		<h3>Данные авторизации</h3>
 		<fieldset class="grid">
 			<label for="email">
 				Электронная почта*
-				<input type="email" name="email" id="email" required />
+				<input type="email" name="email" id="email" bind:value={user.login} required />
 			</label>
 			<label for="password">
 				Пароль*
-				<input type="password" name="password" id="password" required />
+				<input type="password" name="password" id="password" bind:value={user.password} required />
 			</label>
 		</fieldset>
-		<button
-			type="submit"
-			onclick={() => {
-				isVerificationWindowShown = true;
-			}}>Завершить регистрацию</button
-		>
+		<p id="validation-error-warning" bind:this={validationErrorWarning} hidden></p>
+		<p id="other-error-warning" bind:this={otherErrorWarning} hidden></p>
+		{#if promise}
+			{#await promise}
+				<p id="promise-status">Ожидание ответа от сервера</p>
+			{:then}
+				<p id="promise-status" class="green">Запрос успешно обработан</p>
+			{:catch error}
+				{#if error.message}
+					<p id="promise-status" class="red">Произошла непредвиденная ошибка: {error.message}</p>
+				{/if}
+				<button type="submit">Повторить завершение регистрации</button>
+			{/await}
+		{:else}
+			<button type="submit">Завершить регистрацию</button>
+		{/if}
 	</form>
 {:else}
 	<article>
