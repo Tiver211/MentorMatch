9b02e4a3b8e94f57be00267aff07c8a410e38d27
EvdokDima <yoto8644@gmail.com>
2025-03-03 17:52:36 +0500

    add tags in routers

--- api/offer/offer_router.py
+++ api/offer/offer_router.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from .answer_offer.answer_offer import answer_offer_router
 from .post_offer.post_offer import post_offer_router
 
-offer_router = APIRouter()
+offer_router = APIRouter(tags=["Offers"])
 
 offer_router.include_router(answer_offer_router)
 offer_router.include_router(post_offer_router)
