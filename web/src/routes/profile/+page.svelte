f93de053237fa664051206a47bab624aeac9fa75
pakhrom <ro.pakhomov@gmail.com>
2025-03-04 00:37:19 +0300

    feat(web): add buttons in /profile

--- web/src/routes/profile/+page.svelte
+++ web/src/routes/profile/+page.svelte
@@ -1,35 +1,73 @@
-<h1>Управление аккантом</h1>
-<input
-	type="button"
-	value="Выйти из аккаунта"
-	onclick={() => {
-		localStorage.removeItem('isAdmin');
-		localStorage.removeItem('loggedIn');
-		window.location.href = '/';
-	}}
-/>
-<h6 class="red">
-	НЕ ДЛЯ ПРОДА! ЕСЛИ ЭТА КНОПКА ОСТАНЕТСЯ ЗДЕСЬ ПОСЛЕ ДД ВСЕМ РУКИ В БАРАНКУ СВЕРНУ!!!!!!!!!!
-</h6>
-<input
-	type="button"
-	id="hehehe"
-	value="Выдать админку бесплатно, без регистрации и смс"
-	onclick={() => {
-		localStorage.setItem('isAdmin', 'true');
-	}}
-/>
+<script lang="ts">
+	import type { userSchema } from '$lib/schemas';
+	import type { z } from 'zod';
+
+	type User = z.infer<typeof userSchema>;
+
+	let token: string | null = localStorage.getItem('token');
+	let user: User | undefined = $state();
+
+	let promise: any = $state();
+	if (token) {
+		promise = fetch('https://prod-team-35-lg7sic6v.final.prodcontest.ru/api/user/profile', {
+			method: 'GET',
+			headers: {
+				authorization: token
+			}
+		}).then(async (response) => {
+			user = await response.json();
+		});
+	}
+</script>
+
+{#if promise}
+	{#await promise}
+		<h1>Проверка авторизации</h1>
+	{:then}
+		{#if user}
+			<h1>Управление аккантом</h1>
+			<input
+				type="button"
+				value="Редактировать аккаунт"
+				onclick={() => {
+					window.location.href = '/profile/edit';
+				}}
+			/>
+			<input
+				type="button"
+				value="Выйти из аккаунта"
+				onclick={() => {
+					localStorage.removeItem('isAdmin');
+					localStorage.removeItem('loggedIn');
+					window.location.href = '/';
+				}}
+				class="red"
+			/>
+		{/if}
+	{:catch}
+		<h1>Что-то пошло не так</h1>
+		<input
+			type="button"
+			value="Повторить проверку"
+			onclick={() => {
+				window.location.reload();
+			}}
+		/>
+	{/await}
+{:else}
+	<h1>Вы не вошли в акканут</h1>
+	<input
+		type="button"
+		value="Авторизация"
+		onclick={() => {
+			window.location.href = '/login';
+		}}
+	/>
+{/if}
 
 <style>
 	.red {
-		padding: 0.25em;
-		border-radius: 0.25em;
-
-		color: white;
-		background-color: red;
-	}
-
-	#hehehe {
-		background-color: green;
+		background-color: #be3144;
+		border: none;
 	}
 </style>
